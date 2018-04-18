6d24c8c4860709dc996f977b3ff7faa4e5236a8c
phillip-shvartsman <shvartsman.2@osu.edu>
2018-04-18 16:35:52 -0400

    Change UPDOWN Counter to Shifter

--- Ring_Oscillator_Project/up_down.v
+++ Ring_Oscillator_Project/up_down.v
@@ -1,21 +1,21 @@
-module up_down_counter    (
+module up_down_counter #(counterSize = 16)   (
 out      ,  
 up_in  ,
 down_in,  
-clk      , 
+clkUD      , 
 reset       
 );
 //----------Output Ports--------------
-output [15:0] out;
+output [counterSize-1:0] out;
 //------------Input Ports-------------- 
-input up_in, down_in, clk, reset;
+input up_in, down_in, clkUD, reset;
 //------------Internal Variables--------
-reg [15:0] out;
+reg [counterSize-1:0] out;
 wire [1:0] updown;
 assign updown[1] = up_in;
 assign updown[0] = down_in;
 //-------------Code Starts Here-------
-always @(posedge clk) begin
+always @(posedge clkUD) begin
 	if (reset) begin // active high reset
 		out <= 15'd0 ;
 	end
@@ -23,8 +23,14 @@ always @(posedge clk) begin
 		case(updown) 
 			2'b00:out<=out;
 			2'b11:out<=out;
-			2'b01:out<=out-1;
-			2'b10:out<=out+1;
+			2'b01:begin
+				out[counterSize-1] <= 1'b0;
+				out[counterSize-2:0]<=out[counterSize-1:1];
+			end
+			2'b10:begin
+				out[0] <= 1'b1;
+				out[counterSize-1:1]<=out[counterSize-2:0];
+			end
 			default:out<=out;
 		endcase
 	end
